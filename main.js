78adb0e135fff71e4f69672e8f0cbf8d96abed79
Yehor Yefremov <efrem.egor008@gmail.com>
2025-12-23 13:28:30 +0400

    Second commit

--- main.js
+++ main.js
@@ -1,39 +1,118 @@
 const slider = (function () {
-  let index = 0;
+  const state = {
+    duration: 1000,
+    numberOfSlides: 0,
+    elements: {
+      container: null,
+
+      items: null,
+      // points: { container: null, items: null },
+      // buttons: { prev: null, next: null, play: null },
+    },
+    currentIndex: 0,
+
+    increaseIndex() {
+      return ++this.currentIndex;
+    },
+    decreaseIndex() {
+      return --this.currentIndex;
+    },
+    getIndex() {
+      return this.currentIndex;
+    },
+
+    setElements(elements) {
+      this.elements.container = document.querySelector(elements.container);
+      this.elements.items = document.querySelector(elements.items);
+    },
+  };
+
   const items = document.querySelectorAll(".slider__item");
-  const next = document.getElementById("slider-next");
-  const prev = document.getElementById("slider-prev");
+  //   const next = document.getElementById("slider-next");
+  //   const prev = document.getElementById("slider-prev");
+
+  const events = (function () {
+    let prev = null;
+    let next = null;
+    let timer = null;
+
+    let prevFn = null;
+    let nextFn = null;
+
+    const clickEvents = {
+      onPrev(handlePrev) {
+        prevFn = handlePrev;
+        prev.addEventListener("click", prevFn);
+      },
+      onNext(handleNext) {
+        nextFn = handleNext;
+        next.addEventListener("click", nextFn);
+      },
+    };
+
+    const intervalEvents = {
+      start(handleInterval, duration = 1000) {
+        timer = setInterval(handleInterval, duration);
+      },
+      stop() {
+        clearInterval(timer);
+      },
+    };
+
+    const init = () => {
+      next = document.getElementById("slider-next");
+      prev = document.getElementById("slider-prev");
+      return { click: clickEvents, interval: intervalEvents };
+    };
+
+    return { init };
+  })();
 
   const getStep = (index) => {
     return ((index % 5) + 5) % 5;
   };
-  const prevStep = () => getStep(--index);
-  const nextStep = () => getStep(++index);
-  const getCurrentStep = () => getStep(index);
+  const prevStep = () => getStep(state.decreaseIndex());
+  const nextStep = () => getStep(state.increaseIndex());
+  const getCurrentStep = () => getStep(state.getIndex());
 
-  prev.addEventListener("click", () => {
-    const prevIndex = getCurrentStep();
-    prevStep();
-    const currentIndex = getCurrentStep();
+  const eventsInstance = events.init();
 
-    items[prevIndex].style.opacity = 0;
-    items[currentIndex].style.opacity = 1;
+  eventsInstance.click.onPrev(() => {
+    renderSlide(prevStep);
+  });
+  eventsInstance.click.onNext(() => {
+    renderSlide(nextStep);
   });
 
-  next.addEventListener("click", () => {
-    const prevIndex = getCurrentStep();
-    nextStep();
-    const currentIndex = getCurrentStep();
-
-    items[prevIndex].style.opacity = 0;
-    items[currentIndex].style.opacity = 1;
+  eventsInstance.interval.start(() => {
+    renderSlide(nextStep);
   });
 
-  const renderSlide = (action) => {};
+  //   prev.addEventListener("click", () => {
+  //     renderSlide(prevStep);
+  //   });
 
-  setInterval(() => {});
+  //   next.addEventListener("click", () => {
+  //     renderSlide(nextStep);
+  //   });
 
-  const init = () => {};
+  //   setInterval(() => {
+  //     renderSlide(nextStep);
+  //   }, 1000);
 
-  return { init };
+  const renderSlide = (action) => {
+    const prevIndex = getCurrentStep();
+    action();
+    const currentIndex = getCurrentStep();
+
+    state.elements.items[prevIndex].style.opacity = 0;
+    state.elements.items[currentIndex].style.opacity = 1;
+  };
+  const init = (elements) => {
+    state.setElements(elements);
+  };
+
+  return {
+    init,
+  };
 })();
